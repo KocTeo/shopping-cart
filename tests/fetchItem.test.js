af04e6ec30447fad9dae9cc1bb8dc7a0e2054dc2
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-02-02 17:03:43 -0300

    requisito 9

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,29 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  
+  it('Executa fetchItem com argumento MLB1615760527 e verifica se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Testa se o endpoint está correto', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('Verifica se chamada fetchItem com o argumento MLB1615760527 retorna o esperado', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  it('Verifica se chamada fetchItem sem argumento retorna erro', async () => {
+    try {
+      await fetchItem();
+    } catch (error) {
+      expect(error).toEqual('You must provide an url');
+    }
+  });
 });
