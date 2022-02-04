b75da60f513bb29cfe9b8b2d83548755e1a97db6
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-02-04 16:54:44 -0300

    requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-    // seu código aki
+const fetchItem = async (productId) => {
+    const url = `https://api.mercadolibre.com/items/${productId}`;
+    if (url.endsWith('undefined')) {
+      throw new Error('You must provide an url');
+    }
+    const searchItem = await fetch(url);
+    const data = await searchItem.json();
+    return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,5 +3,30 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // seu código aki
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Verifica se o fetch foi chamado', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se o endpoint chamadop é o correto', async () => {
+    await fetchItem('MLB1615760527');
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  it('Verifica se o retorno da função é o esperado', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  it('Verifica se a função sem parametro retorna erro', async () => {
+    try {
+    expect(await fetchItem()).toBe('You must provide an url');
+    } catch (error) {
+      console.log(error);
+    }
+  })
 });
